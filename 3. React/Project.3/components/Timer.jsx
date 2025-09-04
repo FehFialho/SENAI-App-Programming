eb65931d8524b0906b693cf0a98d87d623aba37d
FehFialho <fernandaklechowicz@gmail.com>
2025-09-04 10:16:01 -0300

    Project 3 | Atv 2 - Timer

---
+++ 3. React/Project.3/components/Timer.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import "./Timer.css";
+
+export const Timer = () => {
+    const [currTimer, setTimer] = useState();
+    const [inputValue, setInputValue] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer(prev => {
+                if (prev <= 0) {
+                    clearInterval(interval);
+                    // setMessage("Timer Off");
+                    return 0;
+                }
+                return prev - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const handleStart = () => {
+        const num = parseInt(inputValue, 10);
+        if (!isNaN(num) && num > 0) {
+            setTimer(num);
+        }
+    };
+
+    return (
+        <section className="timer-section">
+            <p className="timer-display">{currTimer}</p>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Insert Seconds"
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    className="timer-input"
+                />
+                <button onClick={handleStart} className="timer-button">
+                    Start
+                </button>
+            </div>
+        </section>
+    );
+};
--- 3. React/Project.3/src/main.jsx
+++ 3. React/Project.3/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Clock } from '../components/Clock.jsx'
+import { Timer } from '../components/Timer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     <Clock></Clock>
+    {/* <Timer></Timer> */}
   </StrictMode>,
 )
