7bfd52c970b94cb442e201fd4767544af5ebaa6a
FehFialho <fernandaklechowicz@gmail.com>
2025-09-17 11:33:15 -0300

    Exercise 4 - Check Users

--- 3. React/C. Routes/src/pages/Profile.jsx
+++ 3. React/C. Routes/src/pages/Profile.jsx
@@ -3,14 +3,27 @@ import { useParams } from "react-router-dom"
 export const Profile = () => {
 
     const { userid } = useParams();
+    const id = parseInt(userid);
+
+    const users = [1,2,4,5,6,7];
+    const isValid = users.includes(id);
+    
+    // for (let i = 0; i < users.length; i++){
+    //     if (users[i] == id)
+    //         isValid = true;
+    //         break;
+    // }
+
+    if (isValid === false) {
+        return <h1>Acesso negado</h1>;
+    }
 
     return(
         <>
             {/* Exercise 1 */}
-            <h1>Profile {userid}</h1>
+            <h1>Profile {id}</h1>
 
-            {/* Exercise 3 */}
-            {userid === "1" ? 
+            {/* Exercise 3 */} {id === 1 ? 
             (
                 <h3>Admin Access</h3>
             ) : 
