395f248dc544f70c77941844f12e7689dd264250
FehFialho <fernandaklechowicz@gmail.com>
2025-09-18 11:39:59 -0300

    Add Challenge

--- 4. TailWind/A. TailWind/src/App.jsx
+++ 4. TailWind/A. TailWind/src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Squares } from './pages/Squares'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-blue-500 text-5xl font-bold'>Testing TailWind!</h1>
-      <div className='h-screen flex justify-center items-center'>
-        <div className='bg-red-900 w-52 h-52'></div>
-        <div className='bg-emerald-800 w-52 h-52'></div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Squares></Squares>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
+    // <>
+    //   <h1 className='text-blue-500 text-5xl font-bold'>Testing TailWind!</h1>
+    //   <div className='h-screen flex justify-center items-center'>
+    //     <div className='bg-red-900 w-52 h-52'></div>
+    //     <div className='bg-emerald-800 w-52 h-52'></div>
+    //   </div>
+    // </>
   )
 }
 
--- 4. TailWind/A. TailWind/src/pages/Squares.jsx
+++ 4. TailWind/A. TailWind/src/pages/Squares.jsx
@@ -1,8 +1,65 @@
 export const Squares = () => {
 
     return(
-        <section>
+        <section className="flex bg-yellow-200 h-screen w-screen flex-col justify-between">
             
+            {/* Line 1 */}
+            <div className="flex w-screen h-[30%] bg-red-400 justify-between items-start">
+                
+                <div className="bg-red-900 h-50 w-50 flex flex-col justify-between items-center">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+
+                <div className="bg-red-900 h-50 w-50 flex justify-center items-center">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+
+                <div className="bg-red-900 h-50 w-50 flex">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+
+                <div className="bg-red-900 h-50 w-50 flex justify-start items-start">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+            </div>
+
+            {/* Line 2 */}
+            <div className="flex w-screen h-[30%] bg-amber-300 justify-between items-center">
+                
+                <div className="bg-red-900 h-50 w-50 flex justify-center items-start">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+
+                <div className="bg-red-900 h-50 w-50 flex">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+                
+                <div className="bg-red-900 h-50 w-50 flex justify-center items-start">
+                    <div className="bg-lime-800 h-15 w-15 m-5"></div>
+                </div>
+            </div>
+
+            {/* Line 3 */}            
+            <div className="flex w-screen h-[30%] bg-green-800 justify-between items-end">
+
+                <div className="bg-red-900 h-50 w-50 flex justify-end items-end">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+
+                <div className="bg-red-900 h-50 w-50 flex justify-center items-center">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+
+                <div className="bg-red-900 h-50 w-50 flex justify-end items-start">
+                    <div className="bg-lime-800 h-15 w-15"></div>
+                </div>
+            </div>
         </section>
     )
 }
