7b53a7edba111f5ad6af0a27fc69702fe8dc8b26
FehFialho <fernandaklechowicz@gmail.com>
2025-09-25 10:09:08 -0300

    Upgrade

--- 3. React/D. ExternalData/src/pages/GetUsers.jsx
+++ 3. React/D. ExternalData/src/pages/GetUsers.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 export const GetUsers = () => {
 
-    const [user, setUser]  = useState([])
+    const [user, setUser] = useState([])
 
     useEffect(() => {
         getUsers();
@@ -12,12 +13,20 @@ export const GetUsers = () => {
     const getUsers = async () => {
         const response = await axios.get('https://jsonplaceholder.typicode.com/users')
         console.log(response.data)
-        console.log(response.data)
+        setUser(response.data)
     }
 
     return(
         <>
-            <h1>TEST</h1>
+            <h1>Registred Users</h1>
+            {
+                user.map(user => (
+                    <li style={{ listStyle: "none"}} key={user.id}>
+                    {user.id} - {user.name}
+                    </li>
+                ))
+            }
+            <Link to={`/`}> <button style={{margin:'2rem'}}>Go Home</button> </Link>
         </>
     )
 }
